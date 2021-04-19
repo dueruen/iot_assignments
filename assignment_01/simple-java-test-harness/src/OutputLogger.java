913633ac683f366e5521d03e0132aa916a48660d
dueruen <32197042+dueruen@users.noreply.github.com>
2021-04-19 11:22:44 +0200

    client logging

--- assignment_01/simple-java-test-harness/src/OutputLogger.java
+++ assignment_01/simple-java-test-harness/src/OutputLogger.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import org.json.*;
 
 class OutputLogger extends Logger {
     BufferedReader input;
@@ -25,5 +26,17 @@ class OutputLogger extends Logger {
         } catch (IOException e) {
             System.out.println("OutputLogger("+filename+") caught finalize exception: "+e);
         }
+    }
+
+    @Override
+    protected void log (String line) throws IOException {
+       JSONObject jo = new JSONObject(new String(line.split(" ")[1]));
+        double time  = jo.getDouble("time");
+        double value = jo.getDouble("value");
+        long current = jo.getLong("current");
+
+        long diff = System.currentTimeMillis() - current;
+
+        writer.write(diff+sep+line+System.lineSeparator()); //+sep+line+System.lineSeparator()
     }
 }
